8761e6058a24ca2d474cc8045992a5ed307666a1
Guillermo Cabral <guille.cabral87@gmail.com>
2021-07-18 20:10:29 -0400

    some message

---
+++ app.js
@@ -0,0 +1,39 @@
+ var a = 2;
+ var b = 3;
+
+ /*Exercice 3*/
+ function sum(a, b) {
+     return sum = ([a] + [b]);
+ }
+ console.log (a + b);
+
+ /*Exercice 4*/
+ function sub(a, b) {
+    return sub = ([a] - [b]);
+}
+console.log (a - b);
+
+ /*Exercice 5*/
+ function mul(a, b) {
+    return mul = ([a] * [b]);
+}
+console.log (a * b);
+
+ /*Exercice 6*/
+ function div(a, b) {
+    return div = ([a] / [b]);
+}
+console.log (a / b);
+
+ /*Exercice 7*/
+sum = () => {[a] + [b]};
+console.log (a + b);
+
+sub = () => {[a] - [b]};
+console.log (a - b);
+
+mul = () => {[a] * [b]};
+console.log (a * b);
+
+div = () => {[a] / [b]};
+console.log (a / b);
